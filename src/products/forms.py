4a41a484b3e8add2f41f371a4cd1bbcf4ae6c101
Hasan Khan <hasan.khan.uap@gmail.com>
2019-01-29 01:52:08 +0600

    new

--- src/products/forms.py
+++ src/products/forms.py
@@ -15,6 +15,23 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField();
-    description = forms.CharField();
-    price = forms.CharField();
+    title = forms.CharField(
+            label='',
+            widget=forms.TextInput(
+                attrs = {
+                    "placeholder": "Your title"
+                }
+            ) 
+        )
+    description = forms.CharField(
+                    required=False, 
+                    widget=forms.Textarea(
+                        attrs = {
+                            "placeholder": "Your description",
+                            "class": "new-class-name",
+                            "rows": 10,
+                            "cols": 120
+                        }
+                    )
+                  )
+    price = forms.CharField(initial=199.99)
