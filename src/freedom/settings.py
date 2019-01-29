c18834d2743eb698c26b0c048a40c2763ad55c4f
Hasan Khan <hasan.khan.uap@gmail.com>
2019-01-30 02:08:43 +0600

    new

--- src/freedom/settings.py
+++ src/freedom/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'blog',
+    'courses',
     'pages',
-    'products'
+    'products',
 ]
 
 MIDDLEWARE = [
--- src/products/views.py
+++ src/products/views.py
@@ -1,49 +1,51 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ProductForm
 from .models import Product
-from .forms import ProductForm, RawProductForm
 
 
 def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == 'POST':
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
     context = {
-        'form': my_form
+        'form': form
     }
-    return render(request, 'products/product_create_view.html', context)
+    return render(request, "products/product_create.html", context)
 
 
-# def product_create_view(request):
-#     #print(request.GET['title'])
-#     print(request.POST)
-#     if request.method == "POST":
-#         new_title = request.POST.get('title')
-#         print(new_title)
-#     context = {}
-#     return render(request, 'products/product_create_view.html', context)
-
-
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
-
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'products/product_create_view.html', context)
-
-
-def product_detail_view(request):
-    obj = Product.objects.get(id=1)
+def product_update_view(request, id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
     context = {
-        'object': obj
+        'form': form
     }
-    return render(request, "products/details.html", context) 
+    return render(request, "products/product_create.html", context)
+
+
+def product_list_view(request):
+    queryset = Product.objects.all() # list of objects
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_delete.html", context)
